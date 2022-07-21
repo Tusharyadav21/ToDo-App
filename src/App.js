3d3019d3088aa79e9097b7bf572cbdd5d1582798
Tusharyadav21 <Tusharydv@hotmail.com>
2022-07-21 16:17:26 +0530

    Code Refactoring

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import { useContext, useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { List } from "./Context";
-import styles from "./App.module.css";
 import { AllTab, ActiveTab, CompletedTab } from "./components/Tab";
+import styles from "./App.module.css";
+
 import sally from "./assets/Saly-22.svg";
-// import Modal from "./components/modal/Modal";
 
 function App() {
 	const [task, setTask] = useState("");
@@ -12,17 +12,12 @@ function App() {
 
 	const { todo, setTodo } = useContext(List);
 
-	// const [modal, setModal] = useState(false);
-	console.log();
-	// const handleModal = () => {
-	// 	setModal(true);
-	// };
-
 	useEffect(() => {
 		const Arr = localStorage.getItem("todos");
 		const parsedArr = JSON.parse(Arr);
 		setTodo(parsedArr);
 		// console.log(parsedArr);
+		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, []);
 
 	function isBlank(str) {
@@ -83,26 +78,9 @@ function App() {
 					</button>
 				</div>
 				<br />
-				{tab === 1 && <AllTab />}
+				{tab === 1 && <AllTab todo={todo} setTodo={setTodo} />}
 				{tab === 2 && <ActiveTab />}
 				{tab === 3 && <CompletedTab />}
-				{/* <div className={styles.add_task} onClick={handleModal}>
-					<svg
-						width='80'
-						height='80'
-						viewBox='0 0 80 80'
-						fill='none'
-						xmlns='http://www.w3.org/2000/svg'
-					>
-						<path
-							fillRule='evenodd'
-							clipRule='evenodd'
-							d='M40 3.33331C19.75 3.33331 3.33331 19.75 3.33331 40C3.33331 60.25 19.75 76.6666 40 76.6666C60.25 76.6666 76.6666 60.25 76.6666 40C76.6666 19.75 60.25 3.33331 40 3.33331ZM43.3333 53.3333C43.3333 54.2174 42.9821 55.0652 42.357 55.6903C41.7319 56.3155 40.884 56.6666 40 56.6666C39.1159 56.6666 38.2681 56.3155 37.643 55.6903C37.0178 55.0652 36.6666 54.2174 36.6666 53.3333V43.3333H26.6666C25.7826 43.3333 24.9347 42.9821 24.3096 42.357C23.6845 41.7319 23.3333 40.884 23.3333 40C23.3333 39.1159 23.6845 38.2681 24.3096 37.643C24.9347 37.0178 25.7826 36.6666 26.6666 36.6666H36.6666V26.6666C36.6666 25.7826 37.0178 24.9347 37.643 24.3096C38.2681 23.6845 39.1159 23.3333 40 23.3333C40.884 23.3333 41.7319 23.6845 42.357 24.3096C42.9821 24.9347 43.3333 25.7826 43.3333 26.6666V36.6666H53.3333C54.2174 36.6666 55.0652 37.0178 55.6903 37.643C56.3155 38.2681 56.6666 39.1159 56.6666 40C56.6666 40.884 56.3155 41.7319 55.6903 42.357C55.0652 42.9821 54.2174 43.3333 53.3333 43.3333H43.3333V53.3333Z'
-							fill='#0075FF'
-						/>
-					</svg>
-				</div> */}
-				{/* {modal && <Modal setModal={setModal} />} */}
 			</div>
 		</div>
 	);
--- src/Context.js
+++ src/Context.js
@@ -5,7 +5,30 @@ export const List = createContext();
 const Context = ({ children }) => {
 	const [todo, setTodo] = useState();
 
-	return <List.Provider value={{ todo, setTodo }}>{children}</List.Provider>;
+	const handleCheckboxChange = (el) => {
+		el.checked = !el.checked;
+		setTodo([...todo]);
+		localStorage.setItem("todos", JSON.stringify([...todo]));
+	};
+
+	const handleRemoveTask = (el) => {
+		const filteredTodo = todo.filter((x) => x.id !== el.id);
+		setTodo([...filteredTodo]);
+		localStorage.setItem("todos", JSON.stringify(filteredTodo));
+	};
+
+	return (
+		<List.Provider
+			value={{
+				todo,
+				setTodo,
+				handleCheckboxChange: handleCheckboxChange,
+				handleRemoveTask: handleRemoveTask,
+			}}
+		>
+			{children}
+		</List.Provider>
+	);
 };
 
 export default Context;
